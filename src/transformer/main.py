a5c0c912af9e4327e2703ac7ebf23a59b722783b
Rahi Iqbal <rahi.iqbal1@gmail.com>
2024-06-27 12:39:54 +0100

    Move files into transformer/ subdirectory

---
+++ src/transformer/main.py
@@ -0,0 +1,33 @@
+import torch
+import torch.nn as nn
+from transformer import Transformer
+
+# Defining constants:
+SOURCE_VOCAB_SIZE = 5000
+TARGET_VOCAB_SIZE = 5000
+MODEL_DIMENSION = 512
+NUM_HEADS = 8
+NUM_LAYERS = 6
+FEED_FORWARD_DIMENSION = 2048
+MAX_SEQ_LENGTH = 100
+DROPOUT = 0.1
+
+def main() -> None:
+    # Initialise transformer:
+    transformer = Transformer(
+        SOURCE_VOCAB_SIZE,
+        TARGET_VOCAB_SIZE,
+        MODEL_DIMENSION,
+        NUM_HEADS,
+        NUM_LAYERS,
+        FEED_FORWARD_DIMENSION,
+        MAX_SEQ_LENGTH,
+        DROPOUT
+    )
+    
+    # Generate random sample data:
+    
+
+
+if __name__ == "__main__":
+    main()
