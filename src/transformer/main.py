e9f578ce42d8b4b5e44bf467700006968bea473a
Rahi Iqbal <rahi.iqbal1@gmail.com>
2024-06-27 13:22:10 +0100

    Write generate random sample data function

--- src/transformer/main.py
+++ src/transformer/main.py
@@ -1,4 +1,5 @@
 import torch
+from torch import Tensor
 import torch.nn as nn
 from transformer_model import Transformer
 
@@ -25,8 +26,26 @@ def main() -> None:
         DROPOUT
     )
     
+    # Get random sample data:
+    source_data: Tensor
+    target_data: Tensor
+    source_data, target_data = generate_random_data()
+
+def generate_random_data(num_examples: int = 64) -> tuple[Tensor, Tensor]:
+    '''
+    Generates random sample data. Simulates a batch of data with num_examples
+    examples (default 64) and sequences of length MAX_SEQ_LENGTH.
+    '''
     # Generate random sample data:
-    
+    source_data: Tensor = torch.randint(
+        1, SOURCE_VOCAB_SIZE, (num_examples, MAX_SEQ_LENGTH) 
+    )
+    target_data: Tensor = torch.randint(
+        1, TARGET_VOCAB_SIZE, (num_examples, MAX_SEQ_LENGTH)
+    )
+
+    return source_data, target_data
+
 
 
 if __name__ == "__main__":
