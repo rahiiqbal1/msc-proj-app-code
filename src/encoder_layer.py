fd2ef5a8756204b0d2378fe324da51e4a0c6d16f
Rahi Iqbal <rahi.iqbal1@gmail.com>
2024-06-27 10:24:39 +0100

    Write forward method for EncoderLayer

--- src/encoder_layer.py
+++ src/encoder_layer.py
@@ -29,5 +29,35 @@ class EncoderLayer(nn.Module):
         # Normalisation layers:
         self.normalise1 = nn.LayerNorm(model_dimension)
         self.normalise2 = nn.LayerNorm(model_dimension)
-        # Dropout:
+        # Dropout layer, randomly sets some activations to 0:
         self.dropout = nn.Dropout(dropout)
+
+    def forward(self, input_tensor: Tensor, mask: Tensor) -> Tensor:
+        '''
+        Takes input_tensor as input to encoder layer and performs a forward
+        pass through.
+        '''
+        # Pass through multi-head self-attention mechanism:
+        attention_output: Tensor = self.self_attention(
+            input_tensor,
+            input_tensor,
+            input_tensor,
+            mask
+        )
+
+        # Attention output is added to the input of this stage (residual),
+        # followed by dropout and normalisation:
+        input_tensor = self.normalise1(
+                input_tensor + self.dropout(attention_output)
+        )
+
+        feed_forward_output: Tensor = self.feed_forward(input_tensor)
+
+        # Feed-forward output is added to the input of this stage (residual),
+        # followed by dropout and normalisation:
+        input_tensor = self.normalise2(
+                input_tensor + self.dropout(feed_forward_output)
+        )
+
+        # Return output of encoder layer:
+        return input_tensor
