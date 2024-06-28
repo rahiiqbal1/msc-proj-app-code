1fa80ab47e19efb8a337de1f3a51e15e753af252
Rahi Iqbal <rahi.iqbal1@gmail.com>
2024-06-28 14:47:28 +0100

    Write generate_jsons_from_ndjsons function

--- src/proof_of_concept/batch_get_embeddings.py
+++ src/proof_of_concept/batch_get_embeddings.py
@@ -1,6 +1,6 @@
 import json
 import os
-from typing import Any
+from typing import Any, Generator
 from txtai import Embeddings
 from tqdm import tqdm
 import joblib
@@ -21,6 +21,9 @@ def main() -> None:
         "reduced-ndjsons"
     )
 
+    for test in generate_jsons_from_ndjsons(wiki_data_path):
+        pass
+
 def load_jsons_from_ndjson(ndjson_file_path: str) -> list[dict[str, Any]]:
     '''
     Loads in json objects from a .ndjson file. Stores as python dictionaries in
@@ -40,6 +43,27 @@ def load_jsons_from_ndjson(ndjson_file_path: str) -> list[dict[str, Any]]:
             pass
 
     return json_list
+
+def generate_jsons_from_ndjsons(
+        ndjsons_dir: str
+        ) -> Generator[list[dict[str, Any]], None, None]:
+    '''
+    Acts as generator, yielding at each step a list of all deserialised json
+    objects from a given file.
+    '''
+    # Get all filenames in ndjsons_dir to read through them:
+    ndjson_filenames: list[str] = os.listdir(ndjsons_dir)
+
+    # Initialising list to store all deserialised jsons:
+    json_list_all: list[dict[str, Any]] = []
+
+    ndjson_filename: str
+    for ndjson_filename in tqdm(ndjson_filenames, "Reading .ndjsons"):
+        # Getting full filepath as ndjson_filename is only the filename:
+        ndjson_filepath: str = os.path.join(ndjsons_dir, ndjson_filename)
+
+        # Yielding list of json dictionaries from the current file:
+        yield load_jsons_from_ndjson(ndjson_filepath)
 
 
 if __name__ == "__main__":
