c893cb5f92fe4ec81ccb0ab9ec7220f622a5347d
Rahi Iqbal <rahi.iqbal1@gmail.com>
2024-06-28 12:16:21 +0100

    Add get_embeddings.py

---
+++ src/proof_of_concept/get_embeddings.py
@@ -0,0 +1,90 @@
+import json
+import os
+from typing import Any
+from txtai import Embeddings
+from tqdm import tqdm
+import joblib
+
+def main() -> None:
+    # Get path to where data is stored. Note that this uses a relative path
+    # from the CWD.
+    data_store_path: str = os.path.join(
+        os.pardir,
+        os.pardir,
+        "data"
+    )
+
+    # Get path to sample data:
+    wiki_data_path: str = os.path.join(
+        data_store_path,
+        "wikidata",
+        "reduced-ndjsons"
+    )
+    
+    # Loading in json data into a list of dictionaries. The data in this sample
+    # has already been reduced from the full data to a more useable format:
+    wiki_jsons: list[dict[str, Any]] = load_all_jsons_from_ndjsons(
+        wiki_data_path
+    )
+
+    # Getting titles to use for indexing: 
+    page_titles: list[str] = []
+    page_json: dict[str, Any]
+    for page_json in wiki_jsons:
+        page_titles.append(page_json["name"])
+    
+    # Generate word embeddings using specified hugging-face model:
+    embeddings = Embeddings(
+        {"path": "sentence-transformers/all-MiniLM-L6-v2"}
+    )
+
+    print(page_titles)
+
+#    # Index page titles and save index:
+#    embeddings.index(page_titles)
+#    embeddings.save(
+#        os.path.join(data_store_path, "wikidata", "0_0_embeddings.tar.gz")
+#    )
+
+def load_jsons_from_ndjson(ndjson_file_path: str) -> list[dict[str, Any]]:
+    '''
+    Loads in json objects from a .ndjson file. Stores as python dictionaries in
+    a python list.
+    '''
+    # Initialising list to store json objects:
+    json_list: list[dict[str, Any]] = []
+    # Opening file:
+    with open(ndjson_file_path, 'r') as ndjson_file:
+        # Reading .ndjson line-by-line, converting each line (json) to python
+        # dictionary, and storing in list:
+        json_line: str
+        try:
+            for json_line in tqdm(ndjson_file, "Loading data"):
+                json_list.append(json.loads(json_line))
+        except json.decoder.JSONDecodeError:
+            pass
+
+    return json_list
+
+def load_all_jsons_from_ndjsons(ndjsons_dir: str) -> list[dict[str, Any]]:
+    '''
+    Loads in all json objects from a directory of .ndjson files. Stores as 
+    python dictionaries in a python list.
+    '''
+    # Get all filenames in ndjsons_dir to read through them:
+    ndjson_filenames: list[str] = os.listdir(ndjsons_dir)
+
+    # Initialising list to store all deserialised jsons:
+    json_list_all: list[dict[str, Any]] = []
+
+    ndjson_filename: str
+    for ndjson_filename in tqdm(ndjson_filenames, "Reading .ndjsons"):
+        # Getting full filepath as ndjson_filename is only the filename:
+        ndjson_filepath: str = os.path.join(ndjsons_dir, ndjson_filename)
+        # Adding resulting list to list of all jsons:
+        json_list_all += load_jsons_from_ndjson(ndjson_filepath)
+
+    return json_list_all
+
+if __name__ == "__main__":
+    main()
---
+++ src/proof_of_concept/main.py
@@ -0,0 +1,9 @@
+from txtai import Embeddings
+import numpy as np
+import pandas as pd
+
+def main() -> None:
+    pass
+
+if __name__ == "__main__":
+    main()
