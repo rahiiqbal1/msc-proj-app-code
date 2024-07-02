2101ae748ba33a31142eea128397d3cf5f09cf85
Rahi Iqbal <rahi.iqbal1@gmail.com>
2024-07-02 14:24:59 +0100

    Working CLI search

--- src/proof_of_concept/main.py
+++ src/proof_of_concept/main.py
@@ -3,7 +3,7 @@ import sys
 from typing import Any
 import joblib
 from txtai import Embeddings
-import streamlit as st
+# import streamlit as st
 
 def main() -> int:
     # Directory where relevant data is stored:
@@ -23,14 +23,38 @@ def main() -> int:
         os.path.join(wikidata_dir, "embeddings_subset_3472510")
     )
 
+    search_query: str = input("Enter a search query: ")
+    num_results: list[tuple[int, float]] = embeddings.search(search_query, 10)
+    readable_results: list[str] = [
+        page_titles[num_result[0]] for num_result in num_results
+    ]
+    for result in readable_results:
+        print(result)
+
     # Page title and search box:
-    st.title("Wikipedia Search Engine")
-    search_query: str = st.text_input("Enter a search query...")
+    # st.title("Wikipedia Search Engine")
+    # search_query: str = st.text_input("Enter a search query...")
+
+    # If the search button is pressed, search through the index with the given
+    # query:
+    # if st.button("Search") == True:
+    #     if search_query == True:
+    #         # num_results is a tuple of the index of the given result in the
+    #         # data and it's score:
+    #         num_results: list[tuple[int, float]]  = embeddings.search(
+    #             search_query, 10
+    #         )
+    #         # Getting readable results by index of result in page_titles:
+    #         readable_results: list[str] = [
+    #             page_titles[num_result[0]] for num_result in num_results
+    #         ]
+    #         for result in readable_results:
+    #             st.write(result)
 
     return 0
 
 # Cache to speed up streamlit load times:
-@st.cache_data
+# @st.cache_data
 def load_data_and_embeddings(
     data_path: str,
     embeddings_path: str
@@ -52,7 +76,7 @@ def load_data_and_embeddings(
     return data, embeddings
 
 # Cache to speed up streamlit load times:
-@st.cache_data
+# @st.cache_data
 def load_data(file_path: str) -> Any:
     '''
     Loads given object as python object using joblib.
