3800c47e1451a6b5336b9a55a2b3ab8e59b77a45
Rahi Iqbal <rahi.iqbal1@gmail.com>
2024-07-02 15:11:55 +0100

    Change get_embeddings.py to use all fields in jsons

--- src/proof_of_concept/get_embeddings.py
+++ src/proof_of_concept/get_embeddings.py
@@ -7,48 +7,48 @@ from txtai import Embeddings
 from tqdm import tqdm
 import joblib
 
-PROPORTION_TITLES_TO_USE = 0.5
+PROPORTION_DATA_TO_USE = 0.3
 
 def main() -> int:
     # Get path to where data is stored. Note that this uses a relative path
     # from the CWD.
-    data_store_path: str = os.path.join(
+    data_store_dir: str = os.path.join(
         os.pardir,
         os.pardir,
         "data"
     )
 
     # Get path to wiki-data:
-    wiki_ndjsons_dir: str = os.path.join(
-        data_store_path,
-        "wikidata",
-        "reduced-ndjsons"
+    wikidata_dir: str = os.path.join(
+        data_store_dir,
+        "wikidata"
     )
 
-    # Only get titles if they have not already been saved:
-    title_save_path: str = os.path.join(
-        os.path.dirname(wiki_ndjsons_dir), "page_titles.gz"
+    # Full path of data file:
+    data_save_path: str = os.path.join(
+        wikidata_dir, "page_titles.gz"
     )
-    page_titles: list[str] = gen_or_get_titles(
-        wiki_ndjsons_dir, title_save_path
+    # Loading in data:
+    data_entries: list[str] = gen_or_get_data(
+        os.path.join(wikidata_dir, "reduced-ndjsons"), data_save_path
     )
 
-    # Using only num_titles_to_use titles to save computation time:
-    num_titles_to_use: int = math.floor(
-        len(page_titles) * PROPORTION_TITLES_TO_USE
+    # Using only num_entries_to_use entries to save computation time:
+    num_entries_to_use: int = math.floor(
+        len(data_entries) * PROPORTION_DATA_TO_USE
     )
-    page_titles_subset: list[str] = page_titles[: num_titles_to_use]
+    data_entries_subset: list[str] = data_entries[: num_entries_to_use]
 
     # If the index does not already exist at the specified path, index and
     # save:
     idx_save_path: str = (
         os.path.join(
-            os.path.dirname(wiki_ndjsons_dir),
-            f"embeddings_subset_{num_titles_to_use}"
+            os.path.dirname(wikidata_dir),
+            f"embeddings_subset_{num_entries_to_use}"
         )
     )
     # Attempt to generate and save index:
-    gen_and_save_index(page_titles_subset, idx_save_path)
+    gen_and_save_index(data_entries_subset, idx_save_path)
 
     return 0
 
@@ -90,35 +90,65 @@ def generate_jsons_from_ndjsons(
         # Yielding list of json dictionaries from the current file:
         yield load_jsons_from_ndjson(ndjson_filepath)
 
-def gen_or_get_titles(
+def gen_or_get_data(
     ndjson_data_dir: str,
-    title_save_path: str
+    data_save_path: str
     ) -> list[str]:
     '''
     If the file at the given path already exists, attempt to load it using
     joblib. If not, read all .ndjson files in the given directory and attempt
-    to read titles from each .json within them.
+    to read name, abstract, categories (todo), and wikitext from each .json 
+    within them.
 
-    Returns the list of titles.
+    Returns a list of all fields for a given entry as a single string, e.g.
+    ["name... abstract... categories... wikitext...", ...]
     '''
-    if os.path.isfile(title_save_path) == False:
-        # Getting titles to use for indexing:
-        page_titles: list[str] = []
-        page_json_list: list[dict[str, Any]]
-        single_page_json: dict[str, Any]
-        for page_json_list in generate_jsons_from_ndjsons(ndjson_data_dir):
-            for single_page_json in page_json_list:
-                page_titles.append(single_page_json["name"])
+    # If the data does not exist, proceed to generate it. Otherwise load it:
+    if os.path.isfile(data_save_path) == False:
+        # Initialising list to store strings of data for each entry:
+        all_entry_data: list[str] = []
 
-        # Save parsed titles at specified save path:
-        title_save_dir, title_save_filename = os.path.split(title_save_path)
-        save_data(page_titles, title_save_filename, title_save_dir)
-        
-        return page_titles 
+        # List of deserialised json objects per .ndjson file read:
+        entry_jsons: list[dict[str, Any]]
+        for entry_jsons in generate_jsons_from_ndjsons(ndjson_data_dir):
+            # Looping over entry_jsons to get single json objects for each 
+            # wikipage entry:
+            entry_json: dict[str, Any]
+            for entry_json in entry_jsons:
+                entry_data: str = ""
+                # String to store data of each field for the given entry:
+                # Looping over each individual json (dictionary) to add each
+                # field to the return string:
+                for field in entry_json:
+                    entry_data += (entry_json[field] + ' ')
+
+                # Adding string of data to list of data:
+                all_entry_data.append(entry_data)
+
+        return all_entry_data
 
     else:
-        page_titles: list[str] = load_data(title_save_path)
-        return page_titles
+        # Loads using joblib (pickle), so the file must be valid as the given
+        # return type:
+        return load_data(data_save_path)
+    # if os.path.isfile(data_save_path) == False:
+    #     # Getting data to use for indexing:
+    #     page_titles: list[str] = []
+    #     page_json_list: list[dict[str, Any]]
+    #     single_page_json: dict[str, Any]
+    #     for page_json_list in generate_jsons_from_ndjsons(ndjson_data_dir):
+    #         for single_page_json in page_json_list:
+    #             page_titles.append(single_page_json["name"])
+
+    #     # Save parsed titles at specified save path:
+    #     title_save_dir, title_save_filename = os.path.split(title_save_path)
+    #     save_data(page_titles, title_save_filename, title_save_dir)
+        
+    #     return page_titles 
+
+    # else:
+    #     page_titles: list[str] = load_data(title_save_path)
+    #     return page_titles
 
 def gen_and_save_index(
     data_to_index: Any,
