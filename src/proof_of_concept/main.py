b1d1182589f81afc335726f8e254115187bd0b6e
Rahi Iqbal <rahi.iqbal1@gmail.com>
2024-07-02 10:58:38 +0100

    Write load_data_and_embeddings and load_data

--- src/proof_of_concept/main.py
+++ src/proof_of_concept/main.py
@@ -1,9 +1,40 @@
+import os
+import sys
+from typing import Any
+import joblib
 from txtai import Embeddings
-import numpy as np
-import pandas as pd
+import streamlit as st
 
-def main() -> None:
-    pass
+def main() -> int:
+
+
+    return 0
+
+def load_data_and_embeddings(
+    data_path: str,
+    embeddings_path: str
+    ) -> tuple[list[str], Embeddings]:
+    '''
+    Loads pickled (with joblib) data and it's associated txtai embeddings.
+
+    Returns a tuple of data and embeddings.
+    '''
+    # Getting data:
+    data: list[str] = load_data(data_path)
+
+    # Getting embeddings:
+    embeddings = Embeddings(
+        {"path": "sentence-transformers/all-MiniLM-L6-v2"}
+    )
+    embeddings.load(embeddings_path)
+
+    return data, embeddings
+
+def load_data(file_path: str) -> Any:
+    '''
+    Loads given object as python object using joblib.
+    '''
+    return joblib.load(file_path)
 
 if __name__ == "__main__":
-    main()
+    sys.exit(main())
