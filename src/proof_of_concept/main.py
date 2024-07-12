06c98babb9e930d4a18fd3b8000b14e084d5e2ce
Rahi Iqbal <rahi.iqbal1@gmail.com>
2024-07-12 10:51:45 +0100

    Modify cli_search to work with dictionaries not strings

--- src/proof_of_concept/main.py
+++ src/proof_of_concept/main.py
@@ -5,7 +5,7 @@ import joblib
 from txtai import Embeddings
 # import streamlit as st
 
-from get_embeddings import NUM_ENTRIES
+from get_data_and_embeddings import NUM_ENTRIES
 
 def main() -> int:
     # Directory where relevant data is stored:
@@ -17,13 +17,14 @@ def main() -> int:
     )
 
     # Get data and saved embeddings:
+    entry_jsons: list[dict[str, str]]
     embeddings: Embeddings
-    entry_data, embeddings = load_data_and_embeddings(
+    entry_jsons, embeddings = load_data_and_embeddings(
         os.path.join(wikidata_dir, "entry_data.gz"),
         os.path.join(wikidata_dir, f"embeddings_subset_{NUM_ENTRIES}")
     )
 
-    cli_search(entry_data, embeddings)
+    cli_search(entry_jsons, embeddings)
 
     return 0
 
@@ -32,14 +33,14 @@ def main() -> int:
 def load_data_and_embeddings(
     data_path: str,
     embeddings_path: str
-    ) -> tuple[list[str], Embeddings]:
+    ) -> tuple[list[dict[str, str]], Embeddings]:
     '''
     Loads data with joblib (pickle) and it's associated txtai embeddings.
 
     Returns a tuple of data and embeddings.
     '''
     # Getting data:
-    data: list[str] = load_data(data_path)
+    data: list[dict[str, str]] = load_data(data_path)
 
     # Getting embeddings:
     embeddings = Embeddings(
@@ -57,33 +58,27 @@ def load_data(file_path: str) -> Any:
     '''
     return joblib.load(file_path)
 
-def cli_search(data: list[str], embeddings: Embeddings) -> None:
+def cli_search(data: list[dict[str, str]], embeddings: Embeddings) -> None:
     '''
     Search through data. uses terminal interface.
     '''
     # Getting query and searching through index:
     search_query: str = input("Enter a search query: ")
-    # This is a list of tuples containing the index of the result in the 
-    # data and it's score:
+    # This is a list of tuples containing the index of the result in the data
+    # and it's score:
     num_results: list[tuple[int, float]] = embeddings.search(search_query, 10)
 
-    # List to store fields for display. Only show a subset of all fields of the
-    # data:
-    results_to_show: list[str] = []
-    # Getting titles of results:
+    # Tuple of desired fields of the data to display to the user in results:
+    fields_to_show: tuple[str, ...] = ("name", "url")
+
+    # Getting results in readable format:
     num_result: tuple[int, float]
     for num_result in num_results:
-        # Getting data from index in num_result:
-        readable_result: str = data[num_result[0]]
-        # Splitting on comma and taking only first element to (roughly) get 
-        # only title:
-        result_title: str = readable_result.split(',')[0]
-        # Adding to list to store data from relevant fields:
-        results_to_show.append(result_title)
-
-    # Displaying results:
-    for result in results_to_show:
-        print(result)
+        readable_result: dict[str, str] = data[num_result[0]]
+        # Displaying only desired fields:
+        field_to_show: str
+        for field_to_show in fields_to_show:
+            print(readable_result[field_to_show])
 
 # def streamlit_search(data: list[str], embeddings: Embeddings) -> None:
 #     ''' 
