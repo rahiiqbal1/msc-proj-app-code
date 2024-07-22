7c93ab6d2e2654cd277c6f77ea95b93fc5e0cae2
Rahi Iqbal <rahi.iqbal1@gmail.com>
2024-07-22 13:44:44 +0100

    Move drawing of search page to a function

--- src/gui_search/qt_gui.py
+++ src/gui_search/qt_gui.py
@@ -1,4 +1,5 @@
 import sys
+import math
 from typing import Callable
 from functools import partial
 from PyQt5.QtCore import Qt
@@ -13,6 +14,8 @@ from PyQt5.QtWidgets import (
     QPushButton,
 )
 
+DISPLAY_WIDTH = 3840
+DISPLAY_HEIGHT = 2160
 WINDOW_WIDTH = 720
 WINDOW_HEIGHT = 480
 WINDOW_TITLE = "Wikipedia Search Engine"
@@ -26,26 +29,42 @@ class SearchWindow(QMainWindow):
     """
     def __init__(self):
         super().__init__()
-        # Attributes:
-        self.generalLayout = QVBoxLayout()
-        self.searchBoxLabel = QLabel()
-        self.searchBox = QLineEdit()
-        self.searchButton = QPushButton(SEARCH_BUTTON_TEXT)
+
+        self.drawSearchPage()
+
+        # Basic window formatting:
+        self.setGeometry(
+            math.floor(0.4 * DISPLAY_WIDTH),
+            math.floor(0.4 * DISPLAY_HEIGHT),
+            WINDOW_WIDTH,
+            WINDOW_HEIGHT
+        )
+        self.setWindowTitle(WINDOW_TITLE)
+
+    def drawSearchPage(self) -> None:
+        """
+        Draws the search page for the window.
+        """
+        # Widgets:
+        generalLayout = QVBoxLayout()
+        searchBoxLabel = QLabel()
+        searchBox = QLineEdit()
+        searchButton = QPushButton(SEARCH_BUTTON_TEXT)
 
         # Formatting for widgets:
-        self.searchBoxLabel.setText(SEARCH_BOX_LABEL)
-        self.searchBox.setAlignment(Qt.AlignmentFlag.AlignLeft)
-        self.searchBox.setFixedHeight(SEARCH_BOX_HEIGHT)
+        searchBoxLabel.setText(SEARCH_BOX_LABEL)
+        searchBox.setAlignment(Qt.AlignmentFlag.AlignLeft)
+        searchBox.setFixedHeight(SEARCH_BOX_HEIGHT)
 
-        self.generalLayout.addWidget(self.searchBoxLabel)
-        self.generalLayout.addWidget(self.searchBox)
-        self.generalLayout.addWidget(self.searchButton)
+        # Adding to layout:
+        generalLayout.addWidget(searchBoxLabel)
+        generalLayout.addWidget(searchBox)
+        generalLayout.addWidget(searchButton)
 
+        # Creating and setting central widget:
         centralWidget = QWidget(self)
-        centralWidget.setLayout(self.generalLayout)
-        # Formatting for main window:
-        self.setGeometry(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT)
-        self.setWindowTitle(WINDOW_TITLE)
+        centralWidget.setLayout(generalLayout)
+
         self.setCentralWidget(centralWidget)
 
 class SearchController:
