22d4823acb8d9dd9aa69d0e74b1e60bd8517bdb9
Rahi Iqbal <rahi.iqbal1@gmail.com>
2024-07-22 10:49:25 +0100

    Write basic window init

---
+++ src/proof_of_concept/qt_gui.py
@@ -0,0 +1,47 @@
+import sys
+from PyQt5.QtWidgets import (
+    QApplication,
+    QMainWindow,
+    QWidget,
+    QGridLayout,
+    QLineEdit,
+    QPushbutton,
+)
+
+WINDOW_WIDTH = 720
+WINDOW_HEIGHT = 480
+
+class SearchWindow(QMainWindow):
+    """
+    Main window for search engine.
+    """
+    def __init__(self, title: str):
+        super().__init__()
+        # Attributes:
+        self.generalLayout = QGridLayout()
+
+        centralWidget = QWidget(self)
+        centralWidget.setLayout(self.generalLayout)
+
+        # Setting window format and widgets:
+        self.setWindowTitle(title)
+        self.setFixedSize(WINDOW_WIDTH, WINDOW_HEIGHT)
+        self.setCentralWidget(centralWidget)
+
+        # self._createDisplay()
+        # self._createButtons()
+
+
+
+def main() -> None:
+    # Initialise app:
+    searchApp = QApplication([])
+    # Initialise window:
+    searchWindow = SearchWindow("test")
+
+    # Display and event loop:
+    searchWindow.show()
+    sys.exit(searchApp.exec())
+
+if __name__ == "__main__":
+    main()
