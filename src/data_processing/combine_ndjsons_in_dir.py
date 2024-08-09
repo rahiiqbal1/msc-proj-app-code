bb8b89c8518e36d4f22403fc8b84d12e1f0b4b9e
Rahi Iqbal <rahi.iqbal1@gmail.com>
2024-08-09 16:52:41 +0100

    Move some things around

---
+++ src/data_processing/combine_ndjsons_in_dir.py
@@ -0,0 +1,39 @@
+import os
+import sys
+
+from tqdm import tqdm
+
+def main() -> None:
+    # Directories to load and save processed data:
+    ndjsons_dir: str = os.path.join(
+        os.pardir, os.pardir, "data", "fully-processed-ndjsons"
+    )
+    combined_ndjson_save_path: str = os.path.join(
+        os.pardir, os.pardir, "data", "combined_processed_ndjson.ndjson"
+    )
+
+    combine_ndjsons_in_dir(ndjsons_dir, combined_ndjson_save_path)
+
+    sys.exit(0)
+
+def combine_ndjsons_in_dir(ndjsons_dir: str, final_save_path: str) -> None:
+    """
+    Combines all .ndjson files in a given directory into a single .ndjson.
+    """
+    # Get all filenames in ndjsons_dir to read through them:
+    ndjson_filenames: list[str] = os.listdir(ndjsons_dir)
+
+    # Opening a file to write the json data to:
+    with open(final_save_path, 'a') as ndjson_to_write_to:
+        ndjson_filename: str
+        for ndjson_filename in tqdm(ndjson_filenames):
+            # Getting full filepath as ndjson_filename is only the filename:
+            ndjson_filepath: str = os.path.join(ndjsons_dir, ndjson_filename)
+
+            # Reading one json at a time from the current ndjson and adding the
+            # line to the final ndjson:
+            with open(ndjson_filepath, 'r') as ndjson_to_read:
+                ndjson_to_write_to.writelines(ndjson_to_read.readlines())
+
+if __name__ == "__main__":
+    main()
