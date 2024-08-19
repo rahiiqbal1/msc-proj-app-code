a8301648aa6e5b7c160fe40c0da6987dcd27330c
Rahi Iqbal <rahi.iqbal1@gmail.com>
2024-08-19 11:31:49 +0100

    Skeleton for bm25GetResults

--- src/gui_search/main.py
+++ src/gui_search/main.py
@@ -86,6 +86,8 @@ def transformerGetResultsSF(
     Searches through given data using given transformer-model-derived
     embeddings. Returns results with all fields intact, so they should be cut
     as desired outside of this function.
+
+    Assumes the data is stored within a single deserialised json object.
     """
     # This is a list of tuples containing the index of the result in the
     # data and it's score:
@@ -101,6 +103,24 @@ def transformerGetResultsSF(
     for numResult in numResults:
         readableResult: dict[str, str] = data[numResult[0]]
         results.append(readableResult)
+
+    return results
+
+# Model:
+def bm25GetResultsSF(
+    jsonData: list[dict[str, str]],
+    dataIndex: dict[str, dict[int, int]],
+    searchQuery: str
+    ) -> list[dict[str, str]]:
+    """
+    Searches through given data using bm25 algorithm. Returns results with all
+    fields intact, so they should be cut as desired outside of this function.
+
+    Assumes that the json data is in the order in which it was indexed. Will 
+    not function correctly if this is not the case.
+    """
+    # Initialising list to store readable results:
+    results: list[dict[str, str]] = []
 
     return results
 
