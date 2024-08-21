f1e942b5f2b9f000f9142504e4a29782685b4570
Rahi Iqbal <rahi.iqbal1@gmail.com>
2024-08-21 10:44:33 +0100

    Fix import in remove_html

--- src/data_processing/remove_html.py
+++ src/data_processing/remove_html.py
@@ -2,9 +2,9 @@ import os
 import sys
 import json
 import copy
-from typing import Any, Generator
 from bs4 import BeautifulSoup
-from tqdm import tqdm
+
+import data_manipulation as dm
 
 def main() -> int:
     # Directory where relevant data is stored:
@@ -31,7 +31,7 @@ def clean_html_from_wikitext(
     unprocessed_json_list: list[dict[str, str]]
     # Variable to keep track of file number when saving:
     file_num: int = 0
-    for unprocessed_json_list in generate_jsons_from_ndjsons(ndjson_dir):
+    for unprocessed_json_list in dm.generate_jsons_from_ndjsons(ndjson_dir):
         # Initialising list to store processed json files per .ndjson:
         processed_json_list: list[str] = []
 
@@ -66,44 +66,6 @@ def clean_html_from_wikitext(
             processed_ndjson.writelines(processed_json_list)
 
         file_num += 1
-
-def load_jsons_from_ndjson(ndjson_file_path: str) -> list[dict[str, Any]]:
-    '''
-    Loads in json objects from a .ndjson file. Stores as python dictionaries in
-    a python list.
-    '''
-# Initialising list to store json objects:
-    json_list: list[dict[str, Any]] = []
-# Opening file:
-    with open(ndjson_file_path, 'r') as ndjson_file:
-        # Reading .ndjson line-by-line, converting each line (json) to python
-        # dictionary, and storing in list:
-        json_line: str
-        try:
-            for json_line in ndjson_file:
-                json_list.append(json.loads(json_line))
-        except json.decoder.JSONDecodeError:
-            pass
-
-    return json_list
-
-def generate_jsons_from_ndjsons(
-    ndjsons_dir: str
-    ) -> Generator[list[dict[str, Any]], None, None]:
-    '''
-    Acts as generator, yielding at each step a list of all deserialised json
-    objects from a given file.
-    '''
-    # Get all filenames in ndjsons_dir to read through them:
-    ndjson_filenames: list[str] = os.listdir(ndjsons_dir)
-
-    ndjson_filename: str
-    for ndjson_filename in tqdm(ndjson_filenames, "Reading .ndjsons"):
-        # Getting full filepath as ndjson_filename is only the filename:
-        ndjson_filepath: str = os.path.join(ndjsons_dir, ndjson_filename)
-
-        # Yielding list of json dictionaries from the current file:
-        yield load_jsons_from_ndjson(ndjson_filepath)
 
 if __name__ == "__main__":
     sys.exit(main())
