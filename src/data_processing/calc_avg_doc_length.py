d0ba072dd9f43353b4a0bb7e5a00f793e09b2a12
Rahi Iqbal <rahi.iqbal1@gmail.com>
2024-08-21 10:26:33 +0100

    Fix bad file path and add status bar

--- src/data_processing/calc_avg_doc_length.py
+++ src/data_processing/calc_avg_doc_length.py
@@ -3,6 +3,7 @@ import os
 from statistics import mean
 
 from nltk.tokenize import word_tokenize
+from tqdm import tqdm
 
 import data_manipulation as dm
 
@@ -13,7 +14,7 @@ def main() -> None:
     )
     # Path to ndjson we wish to check:
     ndjson_file_path: str = os.path.join(
-        wikidata_dir, "poc_combined_processed_ndjson"
+        wikidata_dir, "poc_combined_processed.ndjson"
     )
 
     # Calculating average document length:
@@ -38,7 +39,7 @@ def calc_avg_doc_length_in_ndjson(ndjson_file_path: str) -> float:
 
     # Calculating the length of each json and adding in to the list:
     single_json_str: str
-    for single_json_str in jsons_as_strings:
+    for single_json_str in tqdm(jsons_as_strings):
         json_lengths.append(calc_single_json_length(single_json_str))
 
     # Finding average length and returning:
