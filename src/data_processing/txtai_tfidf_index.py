08facebef3bf51c1291e0baa5c8b7ef500c9ffa4
Rahi Iqbal <rahi.iqbal1@gmail.com>
2024-08-29 09:53:44 +0100

    Rename some files, use txtai tfidf not sklearn

---
+++ src/data_processing/txtai_tfidf_index.py
@@ -0,0 +1,62 @@
+import sys
+import os
+
+from txtai.scoring import ScoringFactory
+from tqdm import tqdm
+
+import data_manipulation as dm
+
+def main() -> None:
+    # Directory in which all application data is stored:
+    wikidata_dir: str = os.path.join(os.pardir, os.pardir, "data")
+
+    # Path to pickled json data which we want to index:
+    pickled_jsons_path: str = os.path.join(
+        wikidata_dir, "poc-fully-processed-ndjsons.pkl"
+    )
+
+    # Full path which we want to save the index at:
+    index_save_path: str = os.path.join(
+        wikidata_dir, "poc_txtai_tfidf_index.pkl"
+    )
+
+    # Fields within the jsons which we want to consider for the index:
+    fields_to_use: tuple[str, ...] = ("name", "abstract")
+
+    # Indexing and saving:
+    txtai_tfidf_index_pickle(
+        pickled_jsons_path, index_save_path, fields_to_use
+    )
+
+    sys.exit(0)
+
+def txtai_tfidf_index_pickle(
+    pickled_jsons_path: str,
+    index_save_path: str,
+    fields_to_use: tuple[str, ...]
+    ) -> None:
+    """
+    Indexes the pickled data at the given path.
+
+    Data must be of the form list[dict[str, str]].
+    """
+    # Loading in data, selecting only desired fields, and converting to
+    # strings:
+    jsons_as_strings_cut: list[str] = dm.stringify_dictionaries(
+        dm.cut_list_of_dicts(
+            dm.load_data(pickled_jsons_path), fields_to_use
+        )
+    )
+
+    # Indexing:
+    tfidf_scoring = ScoringFactory.create({"method": "tfidf", "terms": True})
+    tfidf_scoring.index(
+        tqdm(((i, text, None) for i, text in enumerate(jsons_as_strings_cut)),
+             total = len(jsons_as_strings_cut)
+        )
+    )
+    # Saving. Saves as a pickle:
+    tfidf_scoring.save(index_save_path)
+
+if __name__ == "__main__":
+    main()
